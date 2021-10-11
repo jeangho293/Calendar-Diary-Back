6638f3c0e95e825f657ea763272346036886f948
jeangho <jeangho2932naver.com>
2021-10-11 19:06:02 +0900

    몽고 디비 연결(mongoose)연결 (schema 폴더로 분리)

---
+++ schemas/app.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connect = () => {
+  mongoose
+      .connect('mongodb://localhost:27017/week4Back', {
+        ignoreUndefined: true,
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+};
+
+// connect
+mongoose.connection.on('error', (err) => {
+  console.error('몽고디비 연걸 실패:', err);
+});
+
+console.log('MongoDB ON!');
+module.exports = connect;
