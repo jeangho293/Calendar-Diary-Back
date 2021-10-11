e3831f276a6e6bf1dbe700489004bca2aa1c8e78
jeangho293 <jeangho293@naver.com>
2021-10-11 18:57:03 +0900

    회원가입에 관한 기본 API 구현 --> 조건에 대한 항목은 아직 미구현

---
+++ routers/signup.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../schemas/signup');
+
+router.route('')
+    // 회원가입 페이지에 대한 렌더링 API
+    .get((req, res) => {
+      res.render('signup');
+    })
+
+    // 회원가입 등록에 대한 API
+    .post(async (req, res) => {
+      try {
+        const {userID, PW, confirmPW} = req.body;
+        //console.log(userID, PW, confirmPW);
+        if (await User.findOne({userID})) {
+          res.send({msg: '중복된 아이디입니다.'});
+        } else {
+          await User.create({userID, PW, confirmPW});
+          res.send({msg: '회원가입을 축하드립니다.'});
+        }
+      } catch (err) {
+        console.log(`method: ${req.method}, url: ${req.originalUrl}, error: ${err}`);
+        res.status(400).send({msg: '회원가입 등록 오류입니다.'});
+      }
+      res.end();
+    });
+
+router.route('/checkup')
+    // 회원가입 페이지 중복확인 이벤트에 대한 처리 API
+    .post(async (req, res) => {
+      try {
+        const {userID} = req.body;
+        if (await User.findOne({userID})) {
+          res.send({msg: '중복된 아이디입니다.'});
+        } else {
+          res.send({msg: '사용 가능한 아이디입니다.'});
+        }
+      } catch (err) {
+        console.log(`method: ${req.method}, url: ${req.originalUrl}, error: ${err}`);
+        res.status(400).send({msg: '중복확인 버튼 에러입니다.'});
+      }
+    });
+
+module.exports = router;
