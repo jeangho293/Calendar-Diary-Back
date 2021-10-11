a2af153628d0efb7dc6d0697c1e77ef2b29e1843
jeangho293 <jeangho293@naver.com>
2021-10-12 01:15:05 +0900

    메인페이지 구현 (아직 아무것도 없음)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const homePage = require('./routers/main');
 const loginPage = require('./routers/login');
 const signupPage = require('./routers/signup');
 const connect = require('./schemas/app');
@@ -15,10 +16,11 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET_KEY));
+app.use('/', homePage);
 app.use('/login', loginPage);
 app.use('/signup', signupPage);
 
 // app listening...
 app.listen(process.env.PORT, () => {
-  console.log(`http://localhost:${process.env.PORT}/login`);
+  console.log(`http://localhost:${process.env.PORT}`);
 });
