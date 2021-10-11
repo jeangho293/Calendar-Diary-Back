59b70c7ec4bed12775b98d7d2475bbb9a35ead0c
jeangho293 <jeangho293@naver.com>
2021-10-11 19:03:34 +0900

    몽고DB 연결, login, signup 미들웨어 구성

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-require('dotenv').config()
+const loginPage = require('./routers/login');
+const signupPage = require('./routers/signup');
+const connect = require('./schemas/app');
+connect();
+require('dotenv').config();
 
 // EJS templates language
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 // middleware
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use('/login', loginPage);
+app.use('/signup', signupPage);
 
 // app listening...
-app.listen(process.env.PORT, ()=>{
-  console.log(`http://localhost:${process.env.PORT}`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`http://localhost:${process.env.PORT}/login`);
+});
