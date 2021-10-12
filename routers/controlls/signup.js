eb6b4dd7f13ea6822cdc91ddf1f96a0832043cc1
jeangho293 <jeangho293@naver.com>
2021-10-12 17:10:14 +0900

    ; 콜론 수정 및 send 메세지 수정

--- routers/controlls/signup.js
+++ routers/controlls/signup.js
@@ -1,22 +1,29 @@
 const User = require('../../schemas/signup');
 const bcrypt = require('bcrypt');
 const {CheckRegister} = require('./unit/signup');
+const joi = require('joi');
 
 // 회원가입 절차 기능
 SignUser = async (req, res) => {
+  const UserSchema = joi.object({
+    userID: joi.string().min(4).alphanum().required(),
+    PW: joi.string().min(4).required(),
+    confirmPW: joi.ref('PW'),
+  })
   try {
-    const {userID, PW, confirmPW} = req.body;
+    console.log(await UserSchema.validateAsync(req.body));
+    const {userID, PW, confirmPW} = await UserSchema.validateAsync(req.body);
     const CheckID = await CheckRegister(userID, PW, confirmPW);
 
     if (await User.findOne({userID})) {
-      res.status(200).send({msg: '이미 존재하는 아이디입니다'});
+      res.status(200).send({msg: '이미 존재하는 아이디입니다'})
     } else if (CheckID) {
       res.status(200).send(CheckID);
     } else {
       // bcrypt 를 활용한 비미번화 암호화 및 DB 생성
       const EncryptPW = bcrypt.hashSync(PW, parseInt(process.env.SALT));
       await User.create({userID, PW: EncryptPW});
-      res.status(200).send({msg: '회원가입을 축하드립니다.'});
+      res.status(200).send({msg: 'success'});
     }
   } catch (err) {
     console.log(`method: ${req.method}, url: ${req.originalUrl}, error: ${err}`);
