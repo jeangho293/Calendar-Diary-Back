76d8f7f7ac8fbb6f2b29334b598dfe6ebc906ea8
jeangho <jeangho2932naver.com>
2021-10-12 11:05:47 +0900

    다이어리 router 리팩터링

---
+++ routers/controlls/diary.js
@@ -0,0 +1,42 @@
+const Diary = require('../../schemas/diary');
+
+// 다이어리 작성 기능
+CreateDiary = async (req, res) => {
+  try {
+    const {userID, date, title, content, color} = req.body;
+    // Diary DB저장
+    await Diary.create({userID, date, title, content, color});
+    res.send({msg: 'success'});
+  } catch (err) {
+    console.log(`method: ${req.method}, url: ${req.originalUrl}, err: ${err}`);
+    res.send(500).send({msg: 'fail'});
+  }
+};
+
+// 다이어리 수정 기능
+EditDiary = async (req, res) => {
+  try {
+    const {diaryID, title, content, color} = req.body;
+    await Diary.findByIdAndUpdate(diaryID, {$set: {title, content, color}});
+    res.send({msg: 'success'});
+  } catch (err) {
+    console.log(`method: ${req.method}, url: ${req.originalUrl}, err: ${err}`);
+    res.send(500).send({msg: 'fail'});
+  }
+};
+
+// 다이어리 삭제 기능
+DeleteDiary = async (req, res) => {
+  try{
+
+    await Diary.findByIdAndDelete(req.body.diaryID);
+    res.send({msg: 'success'})
+  } catch (err) {
+    console.log(`method: ${req.method}, url: ${req.originalUrl}, err: ${err}`);
+    res.send(500).send({msg: 'fail'});
+  }
+};
+
+module.exports = {
+  CreateDiary, EditDiary, DeleteDiary,
+}
--- routers/diary.js
+++ routers/diary.js
@@ -1,43 +1,15 @@
 // 다이어리 관련 Router
 const express = require('express');
 const router = express.Router();
-const Diary = require('../schemas/diary');
+
+const {CreateDiary, EditDiary, DeleteDiary} = require('./controlls/diary');
 
 router.route('/')
     // 다이어리 작성
-    .post(async (req, res) => {
-      try {
-        const {
-          userID, date, title, content, color,
-        } = req.body;
-        // DB 저장
-        await Diary.create({userID, date, title, content, color});
-        res.send({msg: '다이어리를 저장했습니다.'});
-      } catch (err) {
-        console.log(`method: ${req.method}, url: ${req.originalUrl}, err: ${err}`);
-        res.send(500).send({msg: '다이어리 작성 에러입니다.'});
-      }
-    })
+    .post(CreateDiary)
     // 다이어리 수정
-    .put(async (req, res) => {
-      try {
-        const {diaryID, title, content, color} = req.body;
-        await Diary.findOneAndUpdate({_id: diaryID}, {$set: {title, content, color}});
-        res.send({msg: '수정완료'});
-      } catch (err) {
-        console.log(`method: ${req.method}, url: ${req.originalUrl}, err: ${err}`);
-        res.status(500).send({msg: '다이어리 수정 오류입니다.'});
-      }
-    })
+    .put(EditDiary)
     // 다이어리 삭제
-    .delete(async (req, res) => {
-      try {
-        const {diaryID} = req.body;
-        await Diary.deleteOne({_id: diaryID});
-        res.send({msg: '삭제되었습니다.'});
-      } catch (err) {
-        console.log(`method: ${req.method}, url: ${req.originalUrl}, err: ${err}`);
-        res.send(500).send({msg: '다이어리 삭제 에러입니다.'});
-      }
-    });
+    .delete(DeleteDiary);
+
 module.exports = router;
