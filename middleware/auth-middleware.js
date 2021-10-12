2461ee9c0e17ebc5f0513e13f31439e9858efc79
jeangho <jeangho2932naver.com>
2021-10-12 09:17:17 +0900

    사용자 인증 미들웨어 구성

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,3 +1,4 @@
+// 사용자 인증 미들웨어
 const jwt = require('jsonwebtoken');
 const User = require('../schemas/signup');
 
@@ -5,12 +6,15 @@ module.exports = async (req, res, next) => {
   const token = req.cookies.mytoken;
 
   try {
+
     if (token) {
+      // 토큰이 존재하면 인증 절차
       const {userID} = jwt.verify(token, process.env.JWT_SECRET_KEY);
       const user = await User.findOne({userID});
       res.locals.user = user.userID;
       next();
     } else {
+      // 토큰이 없으면 튕겨나감
       console.log(`method: ${req.method}, url: ${req.originalUrl}`);
       res.status(400).send({msg: '권한이 없습니다. 로그인 후 사용하세요.'});
       return;
