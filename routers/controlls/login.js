0bbbe52507e6cc1b1c9088b21cd0fb7e6bd854f2
jeangho <jeangho2932naver.com>
2021-10-12 11:26:22 +0900

    로그인 API 리팩터링

---
+++ routers/controlls/login.js
@@ -0,0 +1,35 @@
+const User = require('../../schemas/signup');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+TryLogin = async (req, res) => {
+  try {
+    const {userID, PW} = req.body;
+    const user = await User.findById(userID);
+
+    // DB에 해당 userID가 존재할 경우
+    if (user) {
+      if (await bcrypt.compare(PW, user.PW)) {
+        const token = jwt.sign({userID: user.userID}, process.env.JWT_SECRET_KEY, {
+          expiresIn: '5d',
+        });
+        console.log(`발급된 토큰: ${token}`);
+        res.cookie('mytoken', token, {
+          maxAge: 86400 * 3000,
+          httpOnly: true,
+        });
+        res.status(200).send({msg: 'success'});
+        return;
+      } else {
+        res.status(200).send({msg: '아이디 또는 비밀번호가 틀렸습니다.'});
+      }
+    } else {
+      res.status(200).send({msg: '존재하지 않는 아이디입니다.'});
+    }
+  } catch (err) {
+    console.log(`method: ${req.method}, url: ${req.originalUrl}, error: ${err}`);
+    res.status(400).send({msg: 'fail'});
+  }
+};
+
+module.exports = {TryLogin};
