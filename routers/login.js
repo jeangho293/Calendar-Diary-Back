275f914652d4790743057ee9e94eb7acb1e56d57
jeangho293 <jeangho293@naver.com>
2021-10-12 16:19:28 +0900

    로그인, 회원가입 관련 리팩터링

--- routers/login.js
+++ routers/login.js
@@ -1,46 +1,15 @@
 // 로그인 관련 Router
 const express = require('express');
+const {TryLogin} = require('./controlls/login');
 const router = express.Router();
-const User = require('../schemas/signup');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+
 
 router.route('')
-    // 로그인 페이지 렌더링
-    .get((req, res) => {
-      res.render('login');
-    })
-    // 로그인 API
-    .post(async (req, res) => {
-      try {
-        const {userID, PW} = req.body;
-        const user = await User.findOne({userID});
-
-        // DB에 해당 userID가 존재할 경우
-        if (user) {
-          // 해당 userID의 비밀번호와 req 의 비밀번호가 같은 경우
-          if (await bcrypt.compare(PW, user.PW)) {
-            const token = jwt.sign({userID: user.userID}, process.env.JWT_SECRET_KEY, {
-              expiresIn: '5d',
-            });
-            console.log(token);
-            res.cookie('mytoken', token, {
-              httpOnly: true,
-            });
-            res.status(200).send({msg: 'success'});
-          } else {
-            // 비밀번호가 틀릴 경우
-            res.status(200).send({msg: '아이디 또는 비밀번호가 틀렸습니다.'});
-          }
-        } else {
-          // DB에 해당 userID가 없는 경우
-          res.status(200).send({msg: '존재하지 않는 아이디입니다.'});
-        }
-
-        res.send();
-      } catch (err) {
-        console.log(`method: ${req.method}, url: ${req.originalUrl}, error: ${err}`);
-        res.status(400).send();
-      }
-    });
+    //로그인 페이지 렌더링
+    // .get((req, res) => {
+    //   console.log('로그인페이지 이동했음');
+    //   res.send();
+    // })
+    //로그인 API
+    .post(TryLogin);
 module.exports = router;
--- routers/signup.js
+++ routers/signup.js
@@ -2,50 +2,14 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../schemas/signup');
-const bcrypt = require('bcrypt');
+const {SignUser, CheckDuplicatedID} = require('./controlls/signup');
 
 router.route('')
-    //회원가입 페이지에 대한 렌더링 API
-    .get((req, res) => {
-      console.log('회원가입 페이지로 이동했음')
-      res.send({msg: 'ok'});
-    })
-
     // 회원가입 등록에 대한 API
-    .post(async (req, res) => {
-      try {
-        const {userID, PW, confirmPW} = req.body;
-        console.log(userID, PW, confirmPW);
-        if (await User.findOne({userID})) {
-          console.log('중복');
-          res.send({msg: '중복된 아이디입니다.'});
-        } else {
-          // bcrypt를 활용한 비밀번호 암호화, DB 생성
-          const EncryptPW =  bcrypt.hashSync(PW, parseInt(process.env.SALT));
-          await User.create({userID, PW: EncryptPW});
-          console.log('회원가입 성공');
-          res.send({msg: '회원가입을 축하드립니다.'});
-        }
-      } catch (err) {
-        console.log(`method: ${req.method}, url: ${req.originalUrl}, error: ${err}`);
-        res.status(400).send({msg: '회원가입 등록 오류입니다.'});
-      }
-    });
+    .post(SignUser);
 
 router.route('/checkup')
     // 회원가입 페이지 중복확인 이벤트에 대한 처리 API
-    .post(async (req, res) => {
-      try {
-        const {userID} = req.body;
-        if (await User.findOne({userID})) {
-          res.send({msg: '중복된 아이디입니다.'});
-        } else {
-          res.send({msg: '사용 가능한 아이디입니다.'});
-        }
-      } catch (err) {
-        console.log(`method: ${req.method}, url: ${req.originalUrl}, error: ${err}`);
-        res.status(400).send({msg: '중복확인 버튼 에러입니다.'});
-      }
-    });
+    .post(CheckDuplicatedID);
 
 module.exports = router;
