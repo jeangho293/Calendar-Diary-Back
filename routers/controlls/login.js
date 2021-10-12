f3ac67e2afd1dac1adf356fca86e6e3f7b5960bd
jeangho <jeangho2932naver.com>
2021-10-12 16:18:56 +0900

    cors npm 설치 적용

--- routers/controlls/login.js
+++ routers/controlls/login.js
@@ -5,7 +5,8 @@ const jwt = require('jsonwebtoken');
 TryLogin = async (req, res) => {
   try {
     const {userID, PW} = req.body;
-    const user = await User.findById(userID);
+    console.log(req.body);
+    const user = await User.findOne({userID});
 
     // DB에 해당 userID가 존재할 경우
     if (user) {
@@ -13,16 +14,22 @@ TryLogin = async (req, res) => {
         const token = jwt.sign({userID: user.userID}, process.env.JWT_SECRET_KEY, {
           expiresIn: '5d',
         });
+
         console.log(`발급된 토큰: ${token}`);
         res.cookie('mytoken', token, {
           maxAge: 86400 * 3000,
           httpOnly: true,
+          sameSite: "None",
+          secure: true,
         });
-        res.status(200).send({msg: 'success'});
+        console.log('로그인 성공')
+        res.status(200).json({msg: 'success'});
       } else {
+        console.log('아이디 또는 비밀번호가 틀림')
         res.status(200).send({msg: '아이디 또는 비밀번호가 틀렸습니다.'});
       }
     } else {
+      console.log('존재하지않는 아이디')
       res.status(200).send({msg: '존재하지 않는 아이디입니다.'});
     }
   } catch (err) {
