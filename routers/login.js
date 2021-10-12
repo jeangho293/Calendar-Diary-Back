1413ab448a1c7d18b3e1b24a74c71e50fdb69bb1
jeangho <jeangho2932naver.com>
2021-10-12 19:44:50 +0900

    로그인한 회원이 로그인창 들어왔을 때, 메인페이지로 팅겨내기

--- routers/login.js
+++ routers/login.js
@@ -2,14 +2,18 @@
 const express = require('express');
 const {TryLogin} = require('./controlls/login');
 const router = express.Router();
-
+const authUser = require('../middleware/auth-middleware');
 
 router.route('')
     //로그인 페이지 렌더링
-    // .get((req, res) => {
-    //   console.log('로그인페이지 이동했음');
-    //   res.send();
-    // })
-    //로그인 API
+    .get(authUser, (req, res) => {
+      console.log('로그인페이지 이동했음');
+      if (res.locals.user) {
+        res.send({msg: 'success'});
+      } else {
+        res.send({msg: 'fail'});
+      }
+    })
+
     .post(TryLogin);
 module.exports = router;
