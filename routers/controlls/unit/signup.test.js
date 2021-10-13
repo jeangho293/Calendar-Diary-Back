c96742794fbd8daaadc8fc8782407aca6043f8d6
jeangho293 <jeangho293@naver.com>
2021-10-13 14:36:30 +0900

    회원가입 조건절차 TDD 구현

--- routers/controlls/unit/signup.test.js
+++ routers/controlls/unit/signup.test.js
@@ -6,7 +6,18 @@ const {
   ComparePWandConfirm
 } = require('./signup');
 
-describe('회원가입 조건 통합검사', () => {
+describe('회원가입 절차 조건 검사', () => {
+  test('아래의 조건 통합 검사', () => {
+    expect(CheckRegister('jeangho293', '1234', '1234')).toBeFalsy()
+    expect(CheckRegister('jeangho293', 'hello', 'hello')).toBeFalsy()
+    expect(CheckRegister('JEANGHO293', 'mytoken', 'mytoken')).toBeFalsy()
+    expect(CheckRegister('jea', 'mytoken', 'mytoken')).toBeTruthy()
+    expect(CheckRegister('jea', 'mytoken', 'mytoken')).toBeTruthy()
+    expect(CheckRegister('jea', 'myto', 'mytoken')).toBeTruthy()
+    expect(CheckRegister('jeangho293', 'myto', 'mytoken')).toBeTruthy()
+    expect(CheckRegister('jeangho293', 'myt', 'myt')).toBeTruthy()
+
+  })
   test('아이디는 최소 4글자 이상이며 대소문자, 숫자로만 구성되어집니다.', () => {
     expect(CheckID('jeangho293')).toEqual(true);
     expect(CheckID('JEANGHO293')).toEqual(true);
@@ -27,6 +38,25 @@ describe('회원가입 조건 통합검사', () => {
     expect(CheckPWLength('hi@@')).toEqual(true);
     expect(CheckPWLength('abc')).toEqual(false);
     expect(CheckPWLength('123')).toEqual(false);
-
   });
+
+  test('아이디에 비밀번호가 포함되어있지 않습니다.', () => {
+    expect(CheckIncludePW('jeaghon293', '1234567890')).toEqual(true);
+    expect(CheckIncludePW('jeaghon293', 'jeangho900')).toEqual(true);
+    expect(CheckIncludePW('jeangho2930', 'jago90')).toEqual(true);
+    expect(CheckIncludePW('jeangho293', 'jeangho')).toEqual(false);
+    expect(CheckIncludePW('jeangho2930', '2930')).toEqual(false);
+    expect(CheckIncludePW('jeangho2930', 'ho29')).toEqual(false);
+  });
+
+  test('비밀번호와 재확인 비밀번호가 같습니다.', () => {
+    expect(ComparePWandConfirm('1234657890', '1234657890')).toEqual(true)
+    expect(ComparePWandConfirm('1234', '1234')).toEqual(true)
+    expect(ComparePWandConfirm('jeangho', 'jeangho')).toEqual(true)
+    expect(ComparePWandConfirm('jeangho293', 'jeangho293')).toEqual(true)
+    expect(ComparePWandConfirm('jeangho', 'jeangho293')).toEqual(false)
+    expect(ComparePWandConfirm('jeangho', 'chang')).toEqual(false)
+    expect(ComparePWandConfirm('jeangho293', 'chang293')).toEqual(false)
+    expect(ComparePWandConfirm('123465789', '13579')).toEqual(false)
+  })
 });
