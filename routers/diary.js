1aedda50e91b5acd8c94ea9053476fadc3f2967b
jeangho <jeangho2932naver.com>
2021-10-13 11:48:30 +0900

    다이어리 작성에 대한 사용자 인증 미들웨어 추가

--- routers/diary.js
+++ routers/diary.js
@@ -1,12 +1,12 @@
 // 다이어리 관련 Router
 const express = require('express');
 const router = express.Router();
-
+const authUser = require('../middleware/auth-middleware')
 const {CreateDiary, EditDiary, DeleteDiary} = require('./controlls/diary');
 
 router.route('/')
     // 다이어리 작성
-    .post(CreateDiary)
+    .post(authUser, CreateDiary)
     // 다이어리 수정
     .put(EditDiary)
     // 다이어리 삭제
