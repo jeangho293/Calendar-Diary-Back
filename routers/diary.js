34ed8ae24ef6957c1e3f0b7b26858d77f01a8a35
jeangho <jeangho2932naver.com>
2021-10-13 22:39:33 +0900

    들여쓰기 및 테스트용 get() 요청

--- routers/diary.js
+++ routers/diary.js
@@ -1,15 +1,19 @@
 // 다이어리 관련 Router
 const express = require('express');
 const router = express.Router();
-const authUser = require('../middleware/auth-middleware')
+const authUser = require('../middleware/auth-middleware');
 const {CreateDiary, EditDiary, DeleteDiary} = require('./controlls/diary');
 
 router.route('/')
-    // 다이어리 작성
-    .post(authUser, CreateDiary)
-    // 다이어리 수정
-    .put(EditDiary)
-    // 다이어리 삭제
-    .delete(DeleteDiary);
+  // .get(authUser, (req, res) => {
+  //   console.log(`다이어리 get 요청`);
+  //   res.send('다이어리 get 응답 확인');
+  // })
+  // 다이어리 작성
+  .post(authUser, CreateDiary)
+  // 다이어리 수정
+  .put(EditDiary)
+  // 다이어리 삭제
+  .delete(DeleteDiary);
 
 module.exports = router;
