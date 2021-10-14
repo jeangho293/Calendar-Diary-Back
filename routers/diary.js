7dac127f342a6561f343a631d248ad20424f409a
jeangho <jeangho2932naver.com>
2021-10-14 19:59:39 +0900

    특정 다이어리 get API 구현

--- routers/diary.js
+++ routers/diary.js
@@ -3,16 +3,20 @@ const express = require('express');
 const router = express.Router();
 const authUser = require('../middleware/auth-middleware');
 const {CreateDiary, EditDiary, DeleteDiary} = require('./controlls/diary');
+const Diary = require('../schemas/diary');
 
 router.route('/')
-  // .get(authUser, (req, res) => {
-  //   console.log(`다이어리 get 요청`);
-  //   res.send('다이어리 get 응답 확인');
-  // })
+  // 특정 다이어리 불러오기
+  .get(authUser, async (req, res) => {
+    console.log(req.query);
+    const diaryData = await Diary.find({userID: res.locals.user, date: req.query.date});
+
+    res.json(diaryData);
+  })
   // 다이어리 작성
   .post(authUser, CreateDiary)
   // 다이어리 수정
-  .put(EditDiary)
+  .put(authUser, EditDiary)
   // 다이어리 삭제
   .delete(DeleteDiary);
 
