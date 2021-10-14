ad8e7d97887ccdce1d344c61582a8942627a1bd5
jeangho293 <jeangho293@naver.com>
2021-10-14 13:17:02 +0900

    cookie-parser require 부분 삭제

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser');
 const homePage = require('./routers/main');
 const loginPage = require('./routers/login');
 const signupPage = require('./routers/signup');
@@ -21,7 +20,6 @@ app.use(cors({
 //app.use(morgan('dev'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cookieParser(process.env.JWT_SECRET_KEY));
 app.use('/', homePage);
 app.use('/login', loginPage);
 app.use('/signup', signupPage);
