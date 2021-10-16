0d3ed43b2b0e1d5d5758c23ef0c0613c1f874190
jeangho <jeangho2932naver.com>
2021-10-16 10:04:48 +0900

    앞부문 대문자 교체

--- routers/controlls/unit/error.js
+++ routers/controlls/unit/error.js
@@ -1,4 +1,4 @@
 // error 문구 console.log()
 module.exports = (req, error) => {
-  console.log(`method: ${req.method}, url: ${req.originalUrl}, err: ${error}`);
+  console.log(`METHOD: ${req.method}, URL: ${req.originalUrl}, Error: ${error}`);
 }
